569d566af80b58e83313524b99f0b1314572bc81
JoseGarcia2002 <gonzalezjosemc@gmail.com>
2022-08-11 23:40:28 -0400

    made the backend Heroku friendly

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const generateId = () => {
     const maxId = notes.length > 0
@@ -80,7 +83,7 @@ app.route("/api/notes/:id")
         res.status(204).end()
     })
 
-const PORT = 3000 
+const PORT = process.env.PORT || 3001 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
